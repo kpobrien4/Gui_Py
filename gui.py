9f6a168aa8facdb7b900475015cb09774c9796ac
Kevin O'Brien <kpobrien@buffalo.edu>
2020-02-09 16:34:26 -0500

    simple application

--- gui.py
+++ gui.py
@@ -2,7 +2,8 @@ import PySimpleGUI as sg
 
 sg.theme('DarkBlue')
 
-layout = [  [sg.Text('First GUI')],
+layout = [  [sg.Text('What day is it?')],
+            [sg.Text('Day of the Week'), sg.InputText()],
             [sg.Button('Ok'), sg.Button('Cancel')] ]
 
 window = sg.Window('Window Title', layout)
@@ -11,6 +12,6 @@ while True:
     event, values = window.read()
     if event in (None, 'Cancel'):
         break
-    print('You entered ', values[0])
+    print('Today is', values[0])
 
 window.close()
